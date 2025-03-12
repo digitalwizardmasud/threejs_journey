50be28279f460d4cde8449e52ecdac3de180508d
digitalwizardmasud <masud.pro.121@gmail.com>
2025-03-12 20:54:53 +0600

    r3f debug

--- r3f_c_debug/src/App.jsx
+++ r3f_c_debug/src/App.jsx
@@ -2,12 +2,16 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Experiment from "./components/Experiment";
 import * as THREE from 'three'
+import { Leva } from "leva";
 function App() {
   return (
-    <Canvas>
-      
+     <>
+     <Leva collapsed />
+     
+     <Canvas>
       <Experiment />
     </Canvas>
+    </>
   );
 }
 
--- r3f_c_debug/src/components/Experiment.jsx
+++ r3f_c_debug/src/components/Experiment.jsx
@@ -8,26 +8,67 @@ import {
   TransformControls,
 } from "@react-three/drei";
 import { useFrame, extend, useThree } from "@react-three/fiber";
+import { useControls, button } from "leva";
+import { Perf } from "r3f-perf";
 import React, { useRef } from "react";
 
 const Experiment = () => {
   const cubeRef = useRef();
   const sphereRef = useRef();
+  const sphere = useControls("sphere",{
+    position: {
+      joystick:'invertY',
+      value: {x:2, y:0},
+      min: -10,
+      max: 10,
+      step: 0.1
+    },
+    color: "mediumpurple",
+    visible: true,
+    interval : {
+      min: 0,
+      max: 10,
+      value: [4, 5]
+    },
+    clickme: button(()=> {
+      console.log('clicked')
+    }),
+    choice: {
+      options: ['a', 'b', 'c']
+    }
+
+  })
+
+  const cube = useControls("Cube", {
+    scale: {
+      value: 1.5,
+      min: 0,
+      max: 3,
+      step: 0.1
+    }
+  })
+  const otherOptions = useControls("Other", {
+    perfVisible: false
+  })
+  
   return (
     <>
+      {
+        otherOptions?.perfVisible && <Perf position="top-left" />
+      }
       <OrbitControls makeDefault />
       <directionalLight color={"blue"} intensity={4} position={[1, 2, 3]} />
       <ambientLight intensity={1.5} />
       <group>
       
           {/* Sphere  */}
-          <mesh ref={sphereRef} position-x={2}>
+          <mesh ref={sphereRef} visible={sphere.visible} position={[sphere.position.x, sphere.position.y, 0]} >
             <sphereGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial color={sphere.color} />
           </mesh>
 
         {/* Cube  */}
-        <mesh ref={cubeRef} scale={1.5} position-x={-2}>
+        <mesh ref={cubeRef} scale={cube.scale} position-x={-2}>
           <boxGeometry />
           <meshStandardMaterial color="lightblue" />
         </mesh>
